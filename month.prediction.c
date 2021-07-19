5443901523db62300b4fc2dbc7733e0a27b26910
harsit09 <84085808+harsit09@users.noreply.github.com>
2021-07-19 11:49:35 +0530

    Create month.prediction.c

---
+++ month.prediction.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int month(int);
+void main()
+{
+int x;
+
+printf("\n Input month number: ");
+scanf("%d",&x);
+month(x);
+}
+int month(int m)
+{
+
+switch(m)
+{
+case 1:
+printf("\n January");
+break;
+case 2:
+printf("\n February");
+break;
+case 3:
+printf("\n March");
+break;
+case 4:
+printf("\n April");
+break;
+case 5:
+printf("\n May");
+break;
+case 6:
+printf("\n June");
+break;
+case 7:
+printf("\n July");
+break;
+case 8:
+printf("\n August");
+break;
+case 9:
+printf("\n September");
+break;
+case 10:
+printf("\n October");
+break;
+case 11:
+printf("\n November");
+break;
+case 12:
+printf("\n December");
+break;
+default:
+printf("\n Invalid Input");
+break;
+}
+return 0;
+}
